2bd41469c86971d3a8511f20b2b66d319b81edd1
peppard102 <37004746+peppard102@users.noreply.github.com>
2025-03-29 23:41:14 -0500

    fix(cors): Add cors policy
    * Add cors policy
    
    * gitignore

--- PetCareClinicAPI/Program.cs
+++ PetCareClinicAPI/Program.cs
@@ -1,7 +1,18 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+    policy  =>
+    {
+        policy.WithOrigins("http://localhost:3000");
+    });
+});
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +26,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Enable CORS
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
