ef080b577321193d6bd0816009f8b71d7bb6e575
Lauren Peppard <peppard_lauren@yahoo.com>
2025-03-30 15:06:38 -0500

    Vets controller

---
+++ PetCareClinicAPI/Data/Models/Vets.cs
@@ -0,0 +1,12 @@
+﻿namespace PetCareClinic.Data.Models
+{
+    public class Vet
+    {
+        public int Id { get; set; }
+
+        public required string FirstName { get; set; }
+
+        public required string LastName { get; set; }
+        public required int Age { get; set; }
+    }
+}
