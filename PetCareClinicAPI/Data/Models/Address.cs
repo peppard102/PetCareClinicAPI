a55ba44c218bca1ffcf7e9c3439e73c356d6caba
Lauren Peppard <peppard_lauren@yahoo.com>
2025-04-02 22:09:17 -0500

    pets controller

---
+++ PetCareClinicAPI/Data/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace PetCareClinicAPI.Data.Models
+{
+    public class Address
+    {
+        public required string Street { get; set; }
+
+        public required string City { get; set; }
+
+        public required string State { get; set; }
+
+        public int ZipCode { get; set; }
+    }
+}
